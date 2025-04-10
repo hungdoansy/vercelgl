0e3dc86bfd8091d0221a486d91ce7fd8ca18da1a
Hung Doan <hungdoansy@gmail.com>
2025-04-10 12:50:47 +0700

    update screenshot API

--- api/screenshot.ts
+++ api/screenshot.ts
@@ -1,42 +1,5 @@
-
 const chrome = require('@sparticuz/chromium')
 const puppeteer = require('puppeteer-core')
-
-// const getAbsoluteURL = (hash: string, path?: string) => {
-//   if (!process.env.NODE_ENV) {
-//     return `http://localhost:3000/${hash}`
-//   }
-  
-//   return `https://image.w.kodadot.xyz/ipfs/${path}/${hash}`
-// }
-
-// type ScreenshotRequest = {
-// 	url: string
-// 	settings?: Settings
-// }
-
-// export type Settings = {
-// 	delay?: number;
-// 	width?: number;
-// 	height?: number;
-// }
-
-
-const performCanvasCapture = async (page: any, canvasSelector: string) => {
-  try {
-    // get the base64 image from the CANVAS targetted
-    const base64 = await page.$eval(canvasSelector, el => {
-      if (!el || el.tagName !== "CANVAS") return null
-      return el.toDataURL()
-    })
-    if (!base64) throw new Error("No canvas found")
-    // remove the base64 mimetype at the beginning of the string
-    const pureBase64 = base64.replace(/^data:image\/png;base64,/, "")
-    return Buffer.from(pureBase64, "base64")
-  } catch (err) {
-    return null
-  }
-}
 
 export default async (req: any, res: any) => {
   let {
@@ -67,7 +30,7 @@ export default async (req: any, res: any) => {
 
   if (isProd) {
     browser = await puppeteer.launch({
-      args: chrome.args,
+      args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
       defaultViewport: chrome.defaultViewport,
       executablePath: await chrome.executablePath(),
       headless: 'new',
@@ -82,26 +45,18 @@ export default async (req: any, res: any) => {
 
   const page = await browser.newPage()
 
-  await page.setViewport({ width: 600, height: 600 })
+  await page.setViewport({ width: 1200, height: 630 })
 
   // const url = getAbsoluteURL(`?hash=${hash}`, path)
   const url = body.url
 
   console.log('url', url)
 
-  await page.goto(url);
+  await page.goto(url, {
+    waitUntil: "networkidle2",
+  });
 
-  const selector = 'canvas';
-
-  await page.waitForSelector(selector);
-
-  const element = await performCanvasCapture(page, selector) // const element = page.$(selector)
-
-  // const data = await page.screenshot({
-  //   type: 'png'
-  // })
-
-  const data = element
+  const data = await page.screenshot()
 
   await browser.close()
   // Set the s-maxage property which caches the images then on the Vercel edge
